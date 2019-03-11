06b11cc7b90a7ca526ea3bc7fc1bff5300dd0447
Mike Soares <mike.gsoares@sp.senac.br>
2019-03-11 11:25:24 -0300

    Criação do Projeto

---
+++ VidzMike.API/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VidzMike.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET http://localhost:5000/api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var sArray = new string[] { "Mike", "Soares" };
+            return sArray;
+        }
+
+        // GET http://localhost:5000/api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var sArray = new string[] { "Mike", "Soares" };
+            if (id > (sArray.Length - 1))
+            {
+                return "O valor digitado é maior do que o disponível";
+            }
+
+
+            return sArray[id];
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
